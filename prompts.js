982304fee221f21ec319935b782d3dea66e486c3
lguadarrama770 <134498@students.nccvt.k12.de.us>
2020-11-11 16:17:19 -0500

    prompt one

---
+++ prompts.js
@@ -0,0 +1,48 @@
+/*
+Prompt 1: Write a function, using function keyword, named greeting. 
+When invoked, greeting should print the string 'hey there'.
+*/
+function greeting() {
+    console.log('hey there.');
+}
+
+greeting();
+
+//Prompt 2: Write a function that can reverse a number.
+
+
+//Prompt 3: Write a function that checks whether the passed string is palindrome or not?
+
+
+//Prompt 4: Create a function named alphaOrder that returns a passed string with letters in alphabetical order.
+
+/*
+Prompt 5: Create a function named changeToLowerCase 
+that accepts a string as a parameter and converts letters of each word of the string to lowercase
+*/
+
+/*
+Prompt 6: Make a function that accepts a string as a parameter 
+and converts the first letter of each word of the string in uppercase.
+*/
+
+//Prompt 7: Make a function that accepts a string as a parameter and find the longest word within the string.
+
+/*
+Prompt 8: Create a function that takes two parameters: a and b. 
+When called, multiply a times b. Print the answer to the console.
+*/
+
+//Prompt 9: Make an array of your choosing and write a function that returns the array.
+
+//Prompt 10: Write a function to find the repeated character in a string.
+
+/*
+Funktion- Create a Puppy Age Teller:
+    Write a function named calculateDogAge that:
+        takes 1 argument: your puppy's age.
+        calculates your dog's age based on the conversion rate of 1 human year to 7 dog years.
+        outputs the result to the screen like so: "Your doggie is _ years old in dog years!"
+    Call the function three times with different sets of values.
+
+*/
