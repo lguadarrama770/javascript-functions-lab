409b46e0da6ce73423e147b443f9faa1508b7392
lguadarrama770 <134498@students.nccvt.k12.de.us>
2020-12-02 10:37:33 -0500

    finisheddddd

--- prompts.js
+++ prompts.js
@@ -5,16 +5,30 @@ When invoked, greeting should print the string 'hey there'.
 function greeting() {
     console.log('hey there.');
 }
-
 greeting();
 
 //Prompt 2: Write a function that can reverse a number.
-
+function reversedNumber(num) {
+    return parseFloat(num.toString().split('').reverse().join('') )
+}
 
 //Prompt 3: Write a function that checks whether the passed string is palindrome or not?
-
+function isPalindrome(isIt) {
+    isIt = isIt.toLowerCase();
+    for(let i = 0; i < isIt.length; i++){
+        if(isIt[i] !== isIt[isIt.length - 1 -i]){
+            return false;
+        } else{
+            return true;
+        }
+    }
+}
 
 //Prompt 4: Create a function named alphaOrder that returns a passed string with letters in alphabetical order.
+    function alphaOrder(alpha) {
+        alpha = alpha.toLowerCase();
+        return alpha.split('').sort().join('');
+    }
 
 /*
 Prompt 5: Create a function named changeToLowerCase 
@@ -25,22 +39,57 @@ function changeToLowerCase(){
     return words.toLowerCase();
 }
 
-console.log( changeToLowerCase() );
 /*
 Prompt 6: Make a function that accepts a string as a parameter 
 and converts the first letter of each word of the string in uppercase.
 */
+function firstLetter(firstWord) {
+    let words = firstWord.split(" ")
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
+   return words.join(" ")
+}
 
 //Prompt 7: Make a function that accepts a string as a parameter and find the longest word within the string.
+function longestWord(text) {
+    let sentence = text.split(' ');
+    let maxLength = 0;
+    let result = ' ';
+    for(let i = 0; i <= sentence.length; i++) {
+        if(sentence[i].length > maxLength){
+            maxLength = sentence[i].length;
+            result = sentence[i];
+        }
+    }
+}
 
 /*
 Prompt 8: Create a function that takes two parameters: a and b. 
 When called, multiply a times b. Print the answer to the console.
 */
+function twoParams(a, b){
+    return a*b;
+}
 
 //Prompt 9: Make an array of your choosing and write a function that returns the array.
+let thisIsAnArray = [1,2,3]
+function arr(thisIsAnArray){
+    return thisIsAnArray;
+}
+
 
 //Prompt 10: Write a function to find the repeated character in a string.
+function repeatedCharacter(find) {
+    for(let i = 0; i <= find.length; i++){
+        for(let m = i+1; m <= find.length; m++){
+            if(find[m] === find[i]){
+                return find[i].repeat(1);
+            }
+        }
+    }
+return false;
+}
 
 /*
 Funktion- Create a Puppy Age Teller:
@@ -51,3 +100,8 @@ Funktion- Create a Puppy Age Teller:
     Call the function three times with different sets of values.
 
 */
+function calculateDogAge(puppyAge) {
+    let question = prompt("What is your dog's age?");
+    let puppyAge = question + 7;
+    return 'Your doggie is ' + puppyAge + ' years old in dog years!';
+}
